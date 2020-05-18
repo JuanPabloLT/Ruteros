f025f25b368dfcc80f8d46b683070a26392ec566
JUAN PABLO LONDO�O TOB�N <juanlondono236563@correo.itm.edu.co>
2020-05-18 15:43:07 -0500

    Create warehouses controller, modify button

---
+++ Ruteros.Web/Controllers/WarehousesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Ruteros.Web.Data;
+using Ruteros.Web.Data.Entities;
+
+namespace Ruteros.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class WarehousesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public WarehousesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Warehouses
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Warehouses.ToListAsync());
+        }
+
+       
+        // GET: Warehouses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Warehouses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Telephone")] WarehouseEntity warehouseEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(warehouseEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouseEntity);
+        }
+
+        // GET: Warehouses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var warehouseEntity = await _context.Warehouses.FindAsync(id);
+            if (warehouseEntity == null)
+            {
+                return NotFound();
+            }
+            return View(warehouseEntity);
+        }
+
+        // POST: Warehouses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address,Telephone")] WarehouseEntity warehouseEntity)
+        {
+            if (id != warehouseEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(warehouseEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WarehouseEntityExists(warehouseEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(warehouseEntity);
+        }
+
+        private bool WarehouseEntityExists(int id)
+        {
+            return _context.Warehouses.Any(e => e.Id == id);
+        }
+    }
+}
