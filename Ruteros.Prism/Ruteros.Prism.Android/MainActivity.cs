a4303639dbe37ac65600022c8307e7fb087c226f
MARCOS DAVID DEL RIO RAMIREZ <marcosdel244642@correo.itm.edu.co>
2020-05-31 15:21:57 -0500

    Add splash

--- Ruteros.Prism/Ruteros.Prism.Android/MainActivity.cs
+++ Ruteros.Prism/Ruteros.Prism.Android/MainActivity.cs
@@ -11,7 +11,12 @@ using Syncfusion.SfBusyIndicator.XForms.Droid;
 
 namespace Ruteros.Prism.Droid
 {
-    [Activity(Label = "Ruteros.Prism", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "Ruteros", 
+        Icon = "@mipmap/ic_launcher", 
+        Theme = "@style/MainTheme", 
+        MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
