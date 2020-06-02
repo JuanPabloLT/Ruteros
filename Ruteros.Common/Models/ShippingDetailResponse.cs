a909de8b93883d4e930b75926a2724e9da9bb328
MARCOS DAVID DEL RIO RAMIREZ <marcosdel244642@correo.itm.edu.co>
2020-06-01 21:16:45 -0500

    Fix images in shippings

--- Ruteros.Common/Models/ShippingDetailResponse.cs
+++ Ruteros.Common/Models/ShippingDetailResponse.cs
@@ -7,8 +7,15 @@ namespace Ruteros.Common.Models
     public class ShippingDetailResponse
     {
         public int Id { get; set; }
+
         public int Quantity { get; set; }
+
         public string Description { get; set; }
+
         public string PicturePath { get; set; }
+
+        public string PictureFullPath => string.IsNullOrEmpty(PicturePath)
+        ? "https://ruteroswebitm.azurewebsites.net//images/noimage.png"
+        : $"https://ruteroswebitm.azurewebsites.net{PicturePath.Substring(1)}";
     }
 }
